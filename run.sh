6fd93d43b9ad48409bc705861489617747badf37
stmrym <molly120420@gmail.com>
2024-06-28 18:06:20 +0900

    delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 make_diff_heatmap.py \
-    --img1_dir ../dataset/BSD_3ms24ms/test/%s/Blur/RGB \
-    --img2_dir ../dataset/BSD_3ms24ms/test/%s/Sharp/RGB \
-    --save_path ../dataset/BSD_3ms24ms/diff
