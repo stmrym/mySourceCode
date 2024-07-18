1fc971cd889a6edd9f53a2adf71f1069ce16e045
stmrym <molly120420@gmail.com>
2024-07-18 20:11:26 +0900

    update clip

--- clip_image.py
+++ clip_image.py
@@ -1,61 +1,97 @@
 import os
 import cv2
 import numpy as np
-
-
-path = '/mnt/d/results/20240712/00000123_blur.png'
-save_name = '/mnt/d/results/20240712/2_00000123_blur.png'
-img = cv2.imread(path)
-print(img.shape)
-assert img is not None, 'image empty.'
-
-h, w, _ = img.shape
-# BSD 1
-# x1, y1, w1, h1 = [205, 185, 90, 90]
-# x2, y2, w2, h2 = [230, 385, 100, 70]
-# BSD 2
-# x1, y1, w1, h1 = [110, 60, 320, 130]
-# x2, y2, w2, h2 = [135, 280, 140, 100]
-# GOPRO
-x1, y1, w1, h1 = [425, 210, 45, 45]
-x2, y2, w2, h2 = [290, 0, 130, 60]
-
-color1 = (0,0,255)
-color2 = (0,255,0)
-thick_size = 3
-mode = 'bottom' # 'bottom' or 'right' 
-
-# calculate resize ratio
-if mode == 'bottom':
-    k1 = h2*w / (h2*w1 + h1*w2)
-    k2 = h1*w / (h2*w1 + h1*w2)
-elif mode == 'right':
-    k1 = w2*h / (h1*w2 + h2*w1)
-    k2 = w1*h / (h1*w2 + h2*w1)
-
-# clipping
-img1 = img[y1:y1+h1, x1:x1+w1, :]
-img2 = img[y2:y2+h2, x2:x2+w2, :]
-clip1 = cv2.resize(img1, dsize=None, fx=k1, fy=k1, interpolation=cv2.INTER_NEAREST)
-clip2 = cv2.resize(img2, dsize=None, fx=k2, fy=k2, interpolation=cv2.INTER_NEAREST)
-
-# draw line
-cv2.rectangle(clip1, (1, 1), (clip1.shape[1]-2, clip1.shape[0]-2), color=color1, thickness=thick_size)
-cv2.rectangle(img, (x1, y1, w1, h1), color=color1, thickness=thick_size)
-cv2.rectangle(clip2, (1, 1), (clip2.shape[1]-2, clip2.shape[0]-2), color=color2, thickness=thick_size)
-cv2.rectangle(img, (x2, y2, w2, h2), color=color2, thickness=thick_size)
-
-print(clip1.shape)
-print(clip2.shape)
-
-if mode == 'bottom':
-    bottom = np.hstack((clip1, clip2))
-    out = np.vstack((img, bottom))
-elif mode == 'right':
-    right = np.vstack((clip1, clip2))
-    out = np.hstack((img, right))
-
-cv2.imwrite(save_name, out)
+from typing import List
 
 
 
+def add_patch_inside(path: str, save_name: str, thick_size: int, scale_list: List[float], loc_list: List[str], xywh_list: List[tuple], color_list: List[tuple], **kwargs) -> None:
+    img = cv2.imread(path)
+    assert img is not None, 'image empty.'
+    img_h, img_w, _ = img.shape
+
+    
+    for (x, y, w, h), color, loc, scale in zip(xywh_list, color_list, loc_list, scale_list):
+
+        clip = cv2.resize(img[y:y+h, x:x+w], dsize=None, fx=scale, fy=scale, interpolation=cv2.INTER_NEAREST)
+
+        # draw line
+        cv2.rectangle(clip, (1, 1), (clip.shape[1]-2, clip.shape[0]-2), color=color, thickness=thick_size)
+        cv2.rectangle(img, (x, y, w, h), color=color, thickness=thick_size)
+        clip_h, clip_w, _ = clip.shape
+
+        if loc == 'ul':
+            img[:clip_h, :clip_w] = clip
+        elif loc == 'ur':
+            img[:clip_h:, -clip_w:] = clip
+        elif loc == 'bl':
+            img[-clip_h:, :clip_w] = clip
+        elif loc == 'br':
+            img[-clip_h:, -clip_w:] = clip
+        else:
+            print('invalid loc')
+            exit()
+
+    cv2.imwrite(save_name, img)
+
+
+
+def add_patch_outside(path: str, save_name: str, thick_size: int, xywh_list: List[tuple], color_list: List[tuple], **kwargs) -> None:
+
+    img = cv2.imread(path)
+    assert img is not None, 'image empty.'
+    img_h, img_w, _ = img.shape
+
+    # calculate resize ratio
+    w_list = [xyhw[2] for xyhw in xywh_list]
+    denom = 0
+    for i in range(len(w_list)):
+        # h1*h2*...*wi*...*hn
+        denom += w_list[i] * np.prod([xyhw[3] for j, xyhw in enumerate(xywh_list) if j != i])
+
+    bottom = []
+    # creating patches
+    for i, ((x, y, w, h), color) in enumerate(zip(xywh_list, color_list)):
+        k = img_w * np.prod([xyhw[3] for j, xyhw in enumerate(xywh_list) if j != i]) / denom
+        # clipping
+        clip = cv2.resize(img[y:y+h, x:x+w], dsize=None, fx=k, fy=k, interpolation=cv2.INTER_NEAREST)
+
+        # draw line
+        cv2.rectangle(clip, (1, 1), (clip.shape[1]-2, clip.shape[0]-2), color=color, thickness=thick_size)
+        cv2.rectangle(img, (x, y, w, h), color=color, thickness=thick_size)
+        bottom.append(clip)
+        print(clip.shape)
+
+    stack = np.hstack(bottom)
+    out = np.vstack((img, stack))
+    cv2.imwrite(save_name, out)
+
+
+
+if __name__ == '__main__':
+
+    name = '003012_b'
+    kwargs = {
+        'path' : '/mnt/d/results/20240826_SIP/result_src/%s.png' % name,
+        'save_name' : '/mnt/d/results/20240826_SIP/result_fig/%s.png' % name,
+        'xywh_list' : [
+            # 00000143
+            # (340, 270, 100, 60),
+            # 003012
+            (1000, 60, 150, 140),
+            # 00000020
+            # (255, 200, 170, 90),
+            # (470, 260, 140, 140),
+        ],
+        'color_list': [
+            (0,255,0),
+            (0,0,255),
+            (255,0,0)
+        ],
+        'thick_size' : 4,
+        'scale_list' : [3.5,],
+        'loc_list': ['ul',]
+    }
+
+    # add_patch_outside(**kwargs)
+    add_patch_inside(**kwargs)
