26344e3cc83a9bb9b98a2e68ed381c1ba23548d7
stmrym <molly120420@gmail.com>
2024-10-16 19:15:21 +0900

    make dimension_reduction.yaml

--- dimension_reduction.py
+++ dimension_reduction.py
@@ -1,4 +1,6 @@
+import yaml
 import random
+import os
 import re
 import numpy as np
 from pathlib import Path
@@ -12,58 +14,29 @@ from mpl_toolkits.mplot3d import proj3d
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import plotly.graph_objects as go
 # from MulticoreTSNE import MulticoreTSNE as TSNE
-from sklearn.manifold import TSNE
 from stop_watch import stop_watch
 
-import base64
-from io import BytesIO as _BytesIO
-import time
 
-DATASET_LABEL = ['GOPRO', 'BSD']
+class ImageData():
+    def __init__(self, path, opt):
+        self.opt = opt
+        self.path = path
+        self.image = self._transform(Image.open(path))
+        if self.opt['label_type'] == 'dataset':
+            self.label = self._find_matching_label(path, opt['dir_path_dict'].keys())
+        elif self.opt['label_type'] == 'ssim':
+            self.label = self._find_ssim_from_csv(opt['ssim_csv_path'])
 
-def pil_to_b64(im, enc_format='png', verbose=False, **kwargs):
-    """
-    Converts a PIL Image into base64 string for HTML displaying
-    :param im: PIL Image object
-    :param enc_format: The image format for displaying. If saved the image will have that extension.
-    :return: base64 encoding
-    """
-    t_start = time.time()
-
-    buff = _BytesIO()
-    im.save(buff, format=enc_format, **kwargs)
-    encoded = base64.b64encode(buff.getvalue()).decode("utf-8")
-
-    t_end = time.time()
-    if verbose:
-        print(f"PIL converted to b64 in {t_end - t_start:.3f} sec")
-
-    return encoded
-
-class DimReduction():
-    def __init__(self, dir_path_l, n_sample = 100):
-        self.dir_path_l = dir_path_l
-        self.n_sample = n_sample
-
-        self.path_l = []
-        for dir_path in dir_path_l:
-            paths = sorted([p for p in Path(dir_path).glob('**/*.png') if re.search('blur_gamma|Blur', str(p))])
-            self.path_l += paths
-
-        self.path_l = random.sample(self.path_l, n_sample)
-
-        self.image_l = []
-        self.label_l = []
-        for p in tqdm(self.path_l):
-            self.image_l.append(self._crop_center(Image.open(p)))
-            self.label_l.append(self._find_matching_label(str(p), DATASET_LABEL))
-
-        '''
-        self.path_l: [Path(img1), Path(img2), ...]
-        self.image_l: [(H, W, C), (H, W, C), ...]
-        self.label_l: ['BSD', 'GOPRO', ...]
-        '''
-            
+    def _transform(self, pil_img):
+        if self.opt['input_type'] == 'grayscale':
+            pil_img = pil_img.convert('L')
+        return self._crop_center(pil_img, self.opt.pop('crop_size', 480))
+    
+    def _crop_center(self, pil_img, crop_size):
+        w, h = pil_img.size
+        return pil_img.crop(((w - crop_size) // 2, (h - crop_size) // 2,
+                            (w + crop_size) // 2, (h + crop_size) // 2))
+    
     def _find_matching_label(self, path, label_l):
         for label in label_l:
             if label in path:
@@ -71,24 +44,80 @@ class DimReduction():
         print(f'does not match {path} in {label_l}')
         exit()
 
-    def _crop_center(self, pil_img, crop_size=480):
-        w, h = pil_img.size
-        return pil_img.crop(((w - crop_size) // 2, (h - crop_size) // 2,
-                            (w + crop_size) // 2, (h + crop_size) // 2))
+    def _find_ssim_from_csv(self, csv_path):
+        for dataset_prefix in self.opt['dir_path_dict'].values():
+            if self.path.startswith(dataset_prefix):
+                p = self.path[len(dataset_prefix):].split(os.sep)
+                seq = p[1]
+                frame = os.path.splitext(p[-1])[0]
+                break
+        
+        
+        exit()
+
+class DimReduction():
+    def __init__(self, opt):
+        self.opt = opt
+
+        self.path_l = []
+        for dir_path in self.opt['dir_path_dict'].values():
+            paths = sorted([str(p) for p in Path(dir_path).glob('**/*.png') if re.search('blur_gamma|Blur', str(p))])
+            self.path_l += paths
+
+        self.n_sample = self.opt.pop('n_sample', None)
+        if not self.n_sample == None:
+            self.path_l = random.sample(self.path_l, self.n_sample)
+        else:
+            self.path_l = self.path_l[0:100] # 0 300 3000 3200
+
+        self.image_l = []
+        self.label_l = []
+        for p in tqdm(self.path_l):
+            self.imagedata = ImageData(p, opt)
+            self.image_l.append(self._transform(Image.open(p)))
+            self.label_l.append(self._find_matching_label(p, self.opt['dir_path_dict'].keys()))
+        
+
+            
+
+
+        '''
+        self.path_l: [Path(img1), Path(img2), ...]
+        self.image_l: [(H, W, C), (H, W, C), ...]
+        self.label_l: ['BSD', 'GOPRO', ...] or [SSIM1, SSIM2, ...]
+        '''
+            
+
+
+    def _make_ssim_list_from_csv(self):
+        seq_list = []
+        for path in self.path_l:
+            for dataset_prefix in self.opt['dir_path_dict'].values():
+                if path.startswith(dataset_prefix):
+                    p = path[len(dataset_prefix):].split(os.sep)
+                    print(p)
+                # seq_list += [str(p).lstrip(path) for p in self.path_l]
+        exit()
+        
+        csv_list = [str(p) for p in Path(self.opt['ssim_csv_path']).glob('*.csv')]
+        print(seq_list)
+        print(csv_list)
+        exit()
 
     @stop_watch
-    def fit(self, opt):
+    def fit(self):
         # (N, H, W, C)
-        self.images_np = np.stack(self.image_l, axis=0)
+        images_np = np.stack(self.image_l, axis=0)
         # images_np: (N, CHW)
-        self.images_np = self.images_np.reshape(self.images_np.shape[0], -1)
-        print(self.images_np.shape)
+        images_np = images_np.reshape(images_np.shape[0], -1)
+        print(images_np.shape)
 
-        method = opt.pop('method')  
-        if method == 'tsne':
-            self.tsne = TSNE(n_components=opt['n_components'], perplexity=opt['perplexity'], random_state=opt['random_state'])
-            self.reduced = self.tsne.fit_transform(self.images_np)
-        elif method == 'pca':
+        method = self.opt.pop('method')  
+        if method['name'] == 'tsne':
+            from sklearn.manifold import TSNE
+            tsne = TSNE(n_components=method['n_components'], perplexity=method['perplexity'], random_state=method['random_state'])
+            self.reduced = tsne.fit_transform(images_np)
+        elif method['name'] == 'pca':
             pass
 
         print(self.reduced.shape)
@@ -126,7 +155,7 @@ class Graph():
 
     def _imscatter(self, reduced, image_l, color_l, zoom=0.1):
         # images_np: [T, H, W, C]
-        im_list = [OffsetImage(ImageOps.expand(image, border=20, fill=color), zoom=zoom) for image, color in zip(image_l, color_l)]
+        im_list = [OffsetImage(ImageOps.expand(image.convert('RGB'), border=20, fill=color), zoom=zoom) for image, color in zip(image_l, color_l)]
         x, y = np.atleast_1d(reduced[:,0], reduced[:,1])
         artists = []
 
@@ -179,7 +208,7 @@ class Graph():
             self.ax.autoscale()
             
 
-    def _goscatter(self, reduced, color_l, path_l):
+    def _goscatter(self, reduced, color_l):
         if reduced.shape[1] == 2:
             print('go.Scatter2d mode')
             self.fig = go.Figure(data=[go.Scatter(x=reduced[:,0], y=reduced[:,1], mode='markers', marker=dict(
@@ -190,13 +219,8 @@ class Graph():
             self.fig = go.Figure(data=[go.Scatter3d(x=reduced[:,0], y=reduced[:,1], z=reduced[:,2], mode='markers', marker=dict(
                 size=5, color=color_l
                 ))])
-            
-        if self.graph_opt['add_image']:
-            for x, y, path in zip(reduced[:,0], reduced[:,1], path_l):
-                print(x, y, str(path))
-                self.fig.add_layout_image(dict(source=pil_to_b64(Image.open(path)), xref='x', yref='y', x=x, y=y, sizex=0.5,sizey=0.5))
 
-    def plot(self, reduced, image_l, label_l, path_l):
+    def plot(self, reduced, image_l, label_l):
         '''
         graph_opt: dict
         reduced: (N, d)
@@ -206,17 +230,17 @@ class Graph():
         plot_mode = self.graph_opt.pop('plot_mode')
         color_l = [self.graph_opt['labelcolor'][label] for label in label_l]
 
-        if plot_mode == 'point':
+        if plot_mode == 'plt':
             if self.graph_opt['add_image']:
-                self._imscatter(reduced, image_l, color_l, self.graph_opt.pop('zoom', None))
+                self._imscatter(reduced, image_l, color_l)
             else:
                 self._scatter(reduced, color_l)
         elif plot_mode == 'plotly':
-            self._goscatter(reduced, color_l, path_l)
+            self._goscatter(reduced, color_l)
 
     def save(self):
         if isinstance(self.fig, Mplfig):
-            self.fig.savefig(self.graph_opt['savename'] + '.png', bbox_inches='tight', pad_inches=0, dpi=self.dpi)
+            self.fig.savefig(self.graph_opt['savename'] + '.png', bbox_inches='tight', pad_inches=0.1, dpi=self.dpi)
             print(f'{self.graph_opt["savename"]}.png saved')
         elif isinstance(self.fig, Plotlyfig):
             self.fig.write_html(self.graph_opt['savename'] + '.html')
@@ -226,42 +250,18 @@ class Graph():
 
 if __name__ == '__main__':
 
-    opt = {
-        'dir_path_l':       [
-                            '../dataset/BSD_3ms24ms/test',
-                            '../dataset/GOPRO_Large/test'
-                            ],
-        'seed':             0,
-        'n_sample':         50,
-        'method':           'tsne',
-        'n_components':     2,
-        'random_state':     20,
-        'perplexity':       30,
-        'graph':
-        {
-            'savename':     'tsne_debug',
-            'plot_mode':    'plotly', # 'point' 'imscatter' 'plotly'
-            'add_image':    True,
-            'zoom':         0.05,
-            'figsize':      (8, 8),
-            'dpi':          200,
-            'labelcolor': 
-            {
-                'BSD' :     'magenta',
-                'GOPRO' :   'cyan'
-            }
-        }
-    }
+    with open('dimension_reduction.yaml', 'r') as yml:
+        opt = yaml.safe_load(yml)
 
     random.seed(opt['seed'])
-    dr = DimReduction(opt['dir_path_l'], n_sample = opt['n_sample'])
-    # _ = dr.fit(opt)
+    dr = DimReduction(opt)
+    _ = dr.fit()
 
-    dr.reduced = np.array([[i]*opt['n_components'] for i in range(0,opt['n_sample'])])
-    print(dr.reduced.shape)
+    # dr.reduced = np.array([[i]*opt['n_components'] for i in range(0,opt['n_sample'])])
+    # print(dr.reduced.shape)
 
     graph = Graph(opt)
-    graph.plot(dr.reduced, dr.image_l, dr.label_l, dr.path_l)
+    graph.plot(dr.reduced, dr.image_l, dr.label_l)
     graph.save()
 
 
