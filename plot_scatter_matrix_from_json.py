8c5cb334b9e72fd837c75e7bcceb9daae5bff0e4
stmrym <molly120420@gmail.com>
2025-01-22 08:24:57 +0000

    change name

--- plot_scatter_matrix_from_json.py
+++ plot_scatter_matrix_from_json.py
@@ -45,15 +45,20 @@ def plot_sns(df):
     g = sns.pairplot(df, hue='Seq')
     
     # FR-IQA to blue, NR-IQA to red
-    fr_iqa_l = ['PSNR', 'SSIM', '-LIPIS']
+    fr_iqa_l = ['PSNR', 'SSIM', '-LPIPS']
     nr_iqa_l = ['-NIQE', 'LR']
 
     # Coloring on axis name
     for ax in g.axes.flatten():
         if ax.get_xlabel() in fr_iqa_l:
             ax.set_xlabel(ax.get_xlabel(), color='tab:blue')
-        if ax.get_xlabel() in nr_iqa_l:
+        elif ax.get_xlabel() in nr_iqa_l:
             ax.set_xlabel(ax.get_xlabel(), color='tab:red')
+        
+        if ax.get_ylabel() in fr_iqa_l:
+            ax.set_ylabel(ax.get_ylabel(), color='tab:blue')
+        elif ax.get_ylabel() in nr_iqa_l:
+            ax.set_ylabel(ax.get_ylabel(), color='tab:red')
     
     plt.savefig('scatter_matrix_1200.png', bbox_inches='tight', pad_inches=0.04, dpi=200)
     plt.close()
@@ -64,11 +69,20 @@ def plot_sns(df):
 	fmt='.2f', xticklabels = corr.columns.values, yticklabels = corr.columns.values,)
     
     # Coloring on axis name
-    for ax in g.axes.flatten():
-        if ax.get_xlabel() in fr_iqa_l:
-            ax.set_xlabel(ax.get_xlabel(), color='tab:blue')
-        if ax.get_xlabel() in nr_iqa_l:
-            ax.set_xlabel(ax.get_xlabel(), color='tab:red')
+    x_label_l = g.get_xticklabels()
+    y_label_l = g.get_yticklabels()
+
+    for x_label in x_label_l:
+        if x_label.get_text() in fr_iqa_l:
+            x_label.set_color('tab:blue')
+        elif x_label.get_text() in nr_iqa_l:
+            x_label.set_color('tab:red')
+        
+    for y_label in y_label_l:
+        if y_label.get_text() in fr_iqa_l:
+            y_label.set_color('tab:blue')
+        elif y_label.get_text() in nr_iqa_l:
+            y_label.set_color('tab:red')
     
     plt.savefig('scatter_corr_matrix_1200.png', bbox_inches='tight', pad_inches=0.04, dpi=300)
 
